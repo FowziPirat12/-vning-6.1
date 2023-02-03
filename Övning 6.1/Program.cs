f6ef98cdaade6ad9623d6110ac1e5b05ab8c3339
melvinsjostrand <111962656+Dungeater1337@users.noreply.github.com>
2023-02-03 10:31:12 +0100

    Add project files.

---
+++ Övning 6.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Övning_6._1
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            SkrivTalKvadrat(3);
+            SkrivTalKvadrat(8);
+            SkrivTalKvadrat(15);
+
+        }
+
+        static void SkrivTalKvadrat(int tal)
+        {
+            Console.WriteLine($"Talet{tal} i kvadrat är : {tal * tal}");
+        }
+    }
+}
